e0b17cda9b595cf56e32192a54a8d2bfdb04d5ee
Dtmizu <pietskygenya@gmail.com>
2025-03-18 20:17:09 +0400

    Add files via upload

---
+++ task_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int input;
+    int amount;
+    set<int> numbers;
+
+    cout << "How many numbers?" <<endl;
+    cin >> amount;
+    for (int i = 0; i < amount; i++) {
+        cin >> input;
+        numbers.insert(input);
+    }
+    for (int x : numbers) {
+        cout << x << " ";
+    }
+}
